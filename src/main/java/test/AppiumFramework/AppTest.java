3a96d0331dfeeb73a2fb7e8d590f318cb39d0bf1
ryusuf647 <ryusuf647@hotmail.com>
2020-10-13 20:32:35 -0400

    Third commit

--- src/main/java/test/AppiumFramework/AppTest.java
+++ src/main/java/test/AppiumFramework/AppTest.java
@@ -61,6 +61,7 @@ public class AppTest {
 	public AppiumDriverLocalService startServer() throws InterruptedException	{
 		AppiumServiceBuilder serviceBuilder = new AppiumServiceBuilder();
 		serviceBuilder.withArgument(ArgTest.CHROME_DRIVER, prop.getProperty("chrome_driver"));
+		serviceBuilder.withAppiumJS(new File("C:\\Users\\Basement\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\appium.js"));
 		service = AppiumDriverLocalService.buildService(serviceBuilder);
 		if (!checkIfServerIsRunning(4723))	{
 			service.start();
